a1474fe1cdd1bde865a07785da1a23e072035dd3
JoshuWoshua1 <jkimpear@ucsc.edu>
2025-10-08 16:17:16 +0000

    modified readme and created starting code for button

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,20 @@
 import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
+//Counter variable for button
+let burgers: number = 0;
+
 document.body.innerHTML = `
   <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
+  <p>burgers: <span id="counter">0</span></p>
+  <button id="burgButton">🍔</button>
 `;
+
+// Add click handler
+const button = document.getElementById("burgButton")!;
+const counterElement = document.getElementById("counter")!;
+
+button.addEventListener("click", () => {
+  burgers += 1;
+  counterElement.textContent = burgers.toString();
+});
