b7bd89f2a5c17fe0be910143924a585489a43a88
JoshuWoshua1 <jkimpear@ucsc.edu>
2025-10-10 20:36:35 +0000

    step 5, added a french fries button that adds 1 BPS for 10 burgers (scaling by 1.5x every purchase) BPS counter and upgrades track correctly.

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
-import condiment_1 from "./ketchupSS.png";
+import fries from "./friesSS.png";
+import ketchup from "./ketchupSS.png";
 
 import "./style.css";
 
 //Counter variable for button
 let burgers: number = 0;
-const burgersPerSecond: number = 111;
+let burgersPerSecond: number = 0;
 let clickPower: number = 1;
 
 //variables for Animation
@@ -12,25 +13,31 @@ let lastTime: number = 0;
 const MsPerSecond: number = 1000;
 
 //variables for upgrades
-let ketchupCost: number = 10;
+let ketchupCost: number = 100;
 const ketchupPower: number = 1;
+let friesCost: number = 10;
+const friesPower: number = 1;
 
 document.body.innerHTML = `
   <p>click power: <span id="clickPowerDisplay">${clickPower}</span></p>
-  <p>BPS: <span id="burgersPerSecondDisplay">${burgersPerSecond}</span></p>
+  <p>BPS: <span id="BPSDisplay">${burgersPerSecond}</span></p>
   <h2>burgers: <span id="counter">0</span></h2>
   <button id="burgButton">🍔</button>
-  <p>Cost: <span id="ketchupCostDisplay">${ketchupCost}</span> burgers</p>
-  <button id="ketchupButton"><img src="${condiment_1}" class="icon" /></button>
+  <p>Fries +1 BPS <br>Cost: <span id="friesCostDisplay">${friesCost}</span> burgers</p>
+  <button id="friesButton"><img src="${fries}" class="icon" /></button>
+  <p>Ketchup +1 click power <br>Cost: <span id="ketchupCostDisplay">${ketchupCost}</span> burgers</p>
+  <button id="ketchupButton"><img src="${ketchup}" class="icon" /></button>
 `;
 
 // Add click handler
 const button = document.getElementById("burgButton")!;
 const counterElement = document.getElementById("counter")!;
+const friesButton = document.getElementById("friesButton")!;
+const friesCostDisplay = document.getElementById("friesCostDisplay")!;
 const ketchupButton = document.getElementById("ketchupButton")!;
 const ketchupCostDisplay = document.getElementById("ketchupCostDisplay")!;
 const clickPowerDisplay = document.getElementById("clickPowerDisplay")!;
-//const burgersPerSecondDisplay = document.getElementById("burgersPerSecondDisplay",)!;
+const BPSDisplay = document.getElementById("BPSDisplay")!;
 
 const updateCounterDisplay = () => { //function to make it easier to update current burgers
   counterElement.textContent = Math.floor(burgers).toString(); //changed to allow fractional growth without making it ugly
@@ -41,7 +48,19 @@ button.addEventListener("click", () => {
   updateCounterDisplay();
 });
 
-ketchupButton.addEventListener("click", () => {
+friesButton.addEventListener("click", () => { // new button for step 5, increases BPS instead of click Power
+  if (burgers >= friesCost) {
+    burgers -= friesCost;
+    burgersPerSecond += friesPower;
+    friesCost = Math.floor(friesCost * 1.5);
+
+    updateCounterDisplay();
+    friesCostDisplay.textContent = friesCost.toString();
+    BPSDisplay.textContent = burgersPerSecond.toString();
+  }
+});
+
+ketchupButton.addEventListener("click", () => { // old button
   if (burgers >= ketchupCost) {
     burgers -= ketchupCost;
     clickPower += ketchupPower;
